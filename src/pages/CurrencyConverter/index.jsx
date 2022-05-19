e09990b33ceeef10360f5d4359f3c2775e42a414
Bolio <again.bolio@hotmail.com>
2022-05-18 19:52:32 -0500

    Add the 'MainTitle' component

--- src/pages/CurrencyConverter/index.jsx
+++ src/pages/CurrencyConverter/index.jsx
@@ -3,17 +3,18 @@ import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Divider from "@mui/material/Divider";
 
+import MainTitle from "../../components/title/MainTitle";
+
 function CurrencyConverter() {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-        {/* <MainTitle
-          text={"Currency Converter"}
+        <MainTitle
+          text={"Currency Converter 💱"}
           align={"center"}
           variant={"h3"}
           gutterBottom={true}
-        /> */}
-        <h1>Currency Converter</h1>
+        />
         <Divider />
       </Paper>
     </Container>
