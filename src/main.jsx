9acdcdc512417b33becad042fc97aeda0c033481
Bolio <again.bolio@hotmail.com>
2022-05-19 12:10:05 -0500

    Initial configuration of redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./redux/configureStore";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={configureStore()}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
---
+++ src/redux/configureStore.js
@@ -0,0 +1,27 @@
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+// import currencyConverter from "./modules/currencyConverter/index";
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const composeEnhancers = compose(applyMiddleware(thunk), devTools) || compose;
+
+const rootReducer = () =>
+  combineReducers({
+    currency: currencyConverter,
+  });
+
+export default function configureStore() {
+  const store = createStore(rootReducer(), composeEnhancers);
+
+  // Add more reducers
+  // store.replaceReducer(
+  //   combineReducers({
+  //     currency: currencyConverter,
+  //   })
+  // );
+
+  return store;
+}
