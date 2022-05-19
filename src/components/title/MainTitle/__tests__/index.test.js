4c7077cca2fd34dc8d614768614022969efb4d15
Bolio <again.bolio@hotmail.com>
2022-05-19 14:54:19 -0500

    Change path of babel.config.js file

---
+++ src/components/title/MainTitle/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MainTitle from "..";
+import { render } from "@testing-library/react";
+
+describe("MainTitle component", () => {
+  const component = (
+    <MainTitle
+      text={"Hola Test"}
+      align={"center"}
+      variant={"h3"}
+      gutterBottom={true}
+    />
+  );
+
+  // test("Should render MainTitle component", () => {
+  //   const { container } = render(component);
+  //   expect(container).toMatchSnapshot();
+  // });
+
+  test("Should find a title", () => {
+    const { getByText } = render(component);
+    expect(getByText("Hola Test")).toBeInTheDocument();
+  });
+});
