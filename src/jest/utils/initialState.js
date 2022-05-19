cf8c110984deda74ec48241a2615a5445c758acd
Bolio <again.bolio@hotmail.com>
2022-05-19 17:00:21 -0500

    Add 'renderWithProviderSnapshot' for tests of components connected to redux. Add initialState for tests.

---
+++ src/jest/utils/initialState.js
@@ -0,0 +1,11 @@
+const initialState = {
+  currency: {
+    currentConversion: {},
+    fetching: false,
+    success: false,
+    error: false,
+    errorMessage: "",
+  },
+};
+
+export default initialState;
